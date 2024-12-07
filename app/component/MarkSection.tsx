b2dbe905fb26c22cfd6c29dc011e96348fdd9a22
shakil5281 <shakilhossen307@gmail.com>
2024-12-07 07:00:47 +0600

    Update footer page

--- app/component/MarkSection.tsx
+++ app/component/MarkSection.tsx
@@ -6,7 +6,7 @@ export default function MarkSection() {
         <div className='bg-neutral-300 pt-20 pb-6'>
             <div className='max-w-4xl m-auto flex flex-col justify-center items-center text-neutral-800'>
                 <div className='pb-6'>
-                    <p className='text-5xl font-bold'>Ingredient Obsessed. Chef Crafted.</p>
+                    <p className='lg:text-5xl text-xl font-bold'>Ingredient Obsessed. Chef Crafted.</p>
                 </div>
                 <div className='pb-6'>
                     <p className='text-xl font-semibold text-center'>We believe in the power of whole, real food. We partner with each of our growers, ranchers and producers so that every ingredient meets our TRUE standards. That means better food for you, your family, and the planet.</p>
--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,63 +1,63 @@
+import { Facebook, Instagram, Linkedin } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
+import { FaTiktok } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
 
 export default function Footer() {
     return (
-        <div className='bg-[#525252] py-16'>
-            <div className='max-w-7xl m-auto'>
-                <div className='grid grid-cols-4 gap-10'>
-                    <div>
-                        <div>
-                            <h2 className='text-3xl font-semibold pb-6 text-white'>Title</h2>
-                            <div>
-                                <ul className='text-gray-200 space-y-1 font-normal'>
-                                    <li>Home</li>
-                                    <li>nav</li>
-                                    <li>Fashions</li>
-                                    <li>Menu</li>
-                                    <li>Locations</li>
-                                </ul>
+        <div className='bg-[#3b3d2f] pt-16 pb-36'>
+            <div className='max-w-screen-2xl m-auto'>
+                <div className='md:grid flex flex-col justify-center items-center md:items-start md:grid-cols-3 xl:grid-cols-4 md:px-10'>
+                    {
+                        items.map((item, index) => (
+                            <div key={index}>
+                                <h2 className='text-2xl text-center md:text-left font-semibold pb-6 text-white uppercase'>
+                                    {item.title}
+                                </h2>
+                                <div>
+                                    <ul className='font-normal'>
+                                        {
+                                            item.menu.map((item, index) => (
+                                                <li key={index}>
+                                                    <Link className='text-sm text-center flex flex-col justify-center items-center md:justify-start md:items-start pb-2 text-gray-300 hover:text-gray-100 duration-150' href={item.link}>{item.title}</Link>
+                                                </li>
+                                            ))
+                                        }
+                                    </ul>
+                                </div>
                             </div>
+
+                        ))
+                    }
+                    <div className='md:grid-cols-3 px-8 flex justify-center items-start flex-col w-full'>
+                        <div className='w-full'>
+                            <h2 className='text-3xl font-semibold pb-6 text-white uppercase text-center'>
+                                true INSIDER
+                            </h2>
                         </div>
-                    </div>
-                    <div>
-                        <div>
-                            <h2 className='text-3xl font-semibold pb-6 text-white'>Title</h2>
+                        <div className='w-full'>
+                            <p className='text-gray-200 font-normal text-center'>Get the inside scoop - Join for news, special offers & more</p>
+                        </div>
+                        <div className='flex flex-col justify-center items-center w-full'>
                             <div>
-                                <ul className='text-gray-200 space-y-1 font-normal'>
-                                    <li>Home</li>
-                                    <li>nav</li>
-                                    <li>Fashions</li>
-                                    <li>Menu</li>
-                                    <li>Locations</li>
-                                </ul>
+                                <label className='text-white pb-2' htmlFor="">Email</label>
+                                <input className='w-full p-2 ' type="email" placeholder='Email' />
                             </div>
+                            <div> <button className='text-white px-6 py-2 rounded-3xl text-left' type='submit'>Submit</button></div>
                         </div>
-                    </div>
-                    <div>
-                        <div>
-                            <h2 className='text-3xl font-semibold pb-6 text-white'>Title</h2>
-                            <div>
-                                <ul className='text-gray-200 space-y-1 font-normal'>
-                                    <li>Home</li>
-                                    <li>nav</li>
-                                    <li>Fashions</li>
-                                    <li>Menu</li>
-                                    <li>Locations</li>
-                                </ul>
+                        <div className='w-full'>
+                            <p className='text-white pb-2 text-center'>Connect With Us</p>
+                            <div className='flex flex-row justify-center items-center text-white space-x-4 pb-4'>
+                                <div><FaInstagram size={30} /> </div>
+                                <div><FaLinkedin size={30} /> </div>
+                                <div><FaTiktok size={30} /></div>
+                                <div><FaFacebookF size={30} /> </div>
                             </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div>
-                            <h2 className='text-3xl font-semibold pb-6 text-white'>Title</h2>
-                            <div>
-                                <ul className='text-gray-200 space-y-1 font-normal'>
-                                    <li>Home</li>
-                                    <li>nav</li>
-                                    <li>Fashions</li>
-                                    <li>Menu</li>
-                                    <li>Locations</li>
-                                </ul>
+                            <div className='text-white text-center w-full'>
+                                © True Food Kitchen | Terms & Privacy | Accessibility Statement
                             </div>
                         </div>
                     </div>
@@ -66,3 +66,78 @@ export default function Footer() {
         </div>
     )
 }
+
+
+
+const items = [
+    {
+        title: 'Food',
+        menu: [
+            {
+                title: 'Menu',
+                link: '/menu'
+            },
+            {
+                title: 'Catering',
+                link: '/catering'
+            },
+            {
+                title: 'Nutritionals and Allergens',
+                link: '/nutritionalguide'
+            },
+        ]
+    },
+    {
+        title: 'About',
+        menu: [
+            {
+                title: 'Locations',
+                link: '/locations'
+            },
+            {
+                title: 'Our Story',
+                link: '/catering'
+            },
+            {
+                title: 'Contact',
+                link: '/contact'
+            },
+            {
+                title: 'Careers',
+                link: '/careers'
+            },
+        ]
+    },
+    {
+        title: 'My true food',
+        menu: [
+            {
+                title: 'True Insider',
+                link: '/true-insider'
+            },
+            {
+                title: 'Our Story',
+                link: '/truestory'
+            },
+            {
+                title: 'Gift Cards',
+                link: '/contact'
+            },
+            {
+                title: 'Talk to True',
+                link: '/careers'
+            },
+        ]
+    },
+]
+
+
+
+
+
+
+
+
+
+
+
