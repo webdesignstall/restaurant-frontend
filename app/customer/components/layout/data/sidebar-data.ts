62435ba96bd0a480127a932b748b706cbecd63fa
shakil5281 <shakilhossen307@gmail.com>
2024-12-16 21:16:15 +0600

    manage Order list

---
+++ app/customer/components/layout/data/sidebar-data.ts
@@ -0,0 +1,88 @@
+"use client"
+
+import {
+  IconBarrierBlock,
+  IconBrowserCheck,
+  IconBug,
+  IconChecklist,
+  IconError404,
+  IconHelp,
+  IconLayoutDashboard,
+  IconLock,
+  IconLockAccess,
+  IconMessages,
+  IconNotification,
+  IconPackages,
+  IconPalette,
+  IconServerOff,
+  IconSettings,
+  IconTool,
+  IconUserCog,
+  IconUserOff,
+  IconUsers,
+} from '@tabler/icons-react'
+import { AudioWaveform, Command, GalleryVerticalEnd } from 'lucide-react'
+import { type SidebarData } from '../types'
+
+export const sidebarData: SidebarData = {
+  user: {
+    name: 'satnaing',
+    email: 'satnaingdev@gmail.com',
+    avatar: '/avatars/shadcn.jpg',
+  },
+  teams: [
+    {
+      name: 'Shadcn Admin',
+      logo: Command,
+      plan: 'Vite + ShadcnUI',
+    },
+    {
+      name: 'Acme Inc',
+      logo: GalleryVerticalEnd,
+      plan: 'Enterprise',
+    },
+    {
+      name: 'Acme Corp.',
+      logo: AudioWaveform,
+      plan: 'Startup',
+    },
+  ],
+  navGroups: [
+    {
+      title: 'General',
+      items: [
+        {
+          title: 'Dashboard',
+          url: '/customer/dashboard',
+          icon: IconLayoutDashboard,
+        },
+      ],
+    },
+
+    {
+      title: 'Manage Order',
+      items: [
+        {
+          title: 'Order List',
+          url: '/customer/manage-order/order-list',
+          icon: IconLayoutDashboard,
+        },
+        {
+          title: 'Pending Order',
+          url: '/customer/manage-order/pending-order',
+          icon: IconLayoutDashboard,
+        },
+        {
+          title: 'Complete Order',
+          url: '/customer/manage-order/complete-order',
+          icon: IconLayoutDashboard,
+        },
+        {
+          title: 'Counter Dashboard',
+          url: '/customer/manage-order/counter-board',
+          icon: IconLayoutDashboard,
+        },
+      ],
+    }
+  ],
+}
